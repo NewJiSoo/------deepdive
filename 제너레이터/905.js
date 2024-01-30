3c4a1f90dea7735c928898349707bd1d2daf2255
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-01-30 21:21:44 +0900

    제너레이터 aync await

---
+++ 제너레이터/905.js
@@ -0,0 +1,43 @@
+// 제너레이터 함수 선언문
+function* genDecFunc() {
+    yield 1;
+}
+
+// 제너레이터 함수 표현식
+const genExpFunc = function* () {
+    yield 1;
+};
+
+// 제너레이터 메서드
+const obj = {
+    * genObjMethod() {
+        yield 1;
+    }
+};
+
+// 제너레이터 클래스 메서드
+class MyClass {
+    * genClsMethod() {
+        yield 1;
+    }
+}
+
+
+function* genFunc() { yield 1; }
+
+function* genFunc() { yield 1; }
+
+function* genFunc() { yield 1; }
+
+function* genFunc() { yield 1; }
+
+
+
+const genArrowFunc = * () => {
+    yield 1;
+}; // SyntaxError: Unexpected token '*'
+function* genFunc() {
+    yield 1;
+}
+
+new genFunc(); // TypeError: genFunc is not a constructor
